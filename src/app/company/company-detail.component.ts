eea3759e38bb7b4b462053e9fe89e1fb653ca89e
Molinox <guerreiro.molina@gmail.com>
2021-08-25 04:38:06 -0300

    Alteração e Visuzalição implementados

--- src/app/company/company-detail.component.ts
+++ src/app/company/company-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Company } from '../model/company';
+import { CompanyService } from '../service/company.service';
 
 @Component({
   selector: 'app-company-detail',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompanyDetailComponent implements OnInit {
 
-  constructor() { }
+  company: Company = null;
+
+  constructor(
+    private companyService: CompanyService,
+    private activatedRoute: ActivatedRoute,
+    private toaster: ToastrService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.params.id;
+    this.companyService.consultById(id).subscribe(
+      data => {
+        this.company = data;
+      },
+      err => {
+        this.toaster.error('Não pode exibir os detalhes', 'Fail', {
+          timeOut: 3000,
+        });
+        this.retorno();
+      }
+    );
+  }
+
+  retorno(): void {
+    this.router.navigate(['/'])
   }
 
 }
